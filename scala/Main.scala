0a4c561c9fc7e2b178528dafb366f681dd942a2f
Simon Lagutin <lasimondep@gmail.com>
2019-06-04 02:12:23 +0300

    Небольшие изменения:
    1. Дописал методы SelectSeq для выборки. Теперь сравниваются непосредственно ячейки (Cell).
    2. Начал оформлять сохранение данных в файле.
    
     On branch master
    	modified:   Main.scala
    	modified:   Table.scala

--- scala/Main.scala
+++ scala/Main.scala
@@ -1,18 +1,22 @@
 import scalaDb.{Table, IntegerField, TextField, DoubleField}
 
 object People extends Table {
-  val fields = Map("pk" -> IntegerField(), "name" -> TextField(25), "surname" -> TextField(25), "money" -> DoubleField())
+  val fields = Map("pk" -> IntegerField(), "name" -> TextField(16), "surname" -> TextField(16), "money" -> DoubleField())
+  val filename = "test.bin"
 }
 
 object Main extends App {
-  People.insert(1, "Vasiliy", "Pupkin", 239.17)
+  People.accessOrder
+  People.insert(1, "0123456789abcdef", "0123456789abcdef", 239.17)
   People.insert(2, "Petr", "Vasechkin", 155.0)
   People.insert(3, "Third", "Pupkin", 10.1374)
-  People.accessOrder
-  var res = People.all.equal("surname", "Pupkin")
-
+  var res = People.SelectSeq()
   println("First try")
   res.confirm.foreach(println)
   println("\nSecond try")
-  res.equal("pk", 1).confirm.foreach(println)
+  res = res.lessEq("pk", 2)
+  res.confirm.foreach(println)
+  println("\nThird try")
+  res = res.greaterThan("money", 155.0)
+  res.confirm.foreach(println)
 }
--- scala/Table.scala
+++ scala/Table.scala
@@ -1,4 +1,6 @@
 package scalaDb
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
 
 class Field
 case class IntegerField() extends Field
@@ -7,6 +9,26 @@ case class TextField(val maxLen: Int) extends Field
 
 abstract class Cell {
   def toString: String
+  def <(other: Any): Boolean = (this, other) match {
+    case (CellInt(x), y: Int) => x < y
+    case (CellDouble(x), y: Double) => x < y
+    case (CellText(x), y: String) => x < y
+  }
+  def >(other: Any): Boolean = (this, other) match {
+    case (CellInt(x), y: Int) => x > y
+    case (CellDouble(x), y: Double) => x > y
+    case (CellText(x), y: String) => x > y
+  }
+  def <=(other: Any): Boolean = (this, other) match {
+    case (CellInt(x), y: Int) => x <= y
+    case (CellDouble(x), y: Double) => x <= y
+    case (CellText(x), y: String) => x <= y
+  }
+  def >=(other: Any): Boolean = (this, other) match {
+    case (CellInt(x), y: Int) => x >= y
+    case (CellDouble(x), y: Double) => x >= y
+    case (CellText(x), y: String) => x >= y
+  }
 }
 case class CellInt(val x: Int) extends Cell {
   override def toString: String = x.toString
@@ -18,34 +40,62 @@ case class CellText(val x: String) extends Cell {
   override def toString: String = x.toString
 }
 
-case class Row(val data: List[Cell] = Nil, val pos: Int = 0) {
-  def save: Unit = {
-    println(s"Row #$pos saved: " + data.toString) //TODO
-  }
-  override def toString: String = data.mkString(", ")
-}
-
 abstract class Table {
   var data: List[Row] = Nil
   val fields: Map[String, Field]
-  var fieldOrder: List[String] = Nil
-  def accessOrder: Unit = fieldOrder = fields.keys.toList
+  var fieldOrder: Array[String] = Array()
+  def accessOrder: Unit = fieldOrder = fields.keys.toArray
+  def filename: String
 
-  case class SelectSeq(val prev: List[SelectSeq], val mod: Row => Boolean) {
+  //TODO: Продумать и дописать общение с физическим файлом
+  case class Row(val data: Array[Cell], val pos: Int = 0) {
+    def save: Unit = {
+      var capacity = 8
+      for (it <- fieldOrder)
+        capacity = capacity + (fields(it) match {
+          case IntegerField() => 4
+          case DoubleField() => 8
+          case TextField(l) => l * 2
+        })
+      var rawData  = ByteBuffer.allocate(capacity)
+      for (i <- 0 until fieldOrder.length) {
+        (fields(fieldOrder(i)), data(i)) match {
+          case (IntegerField(), CellInt(x)) => rawData.putInt(1).putInt(4).putInt(x)
+          case (DoubleField(), CellDouble(x)) => rawData.putInt(2).putInt(8).putDouble(x)
+          case (TextField(l), CellText(s)) => rawData.putInt(l).putInt(l).put(s.getBytes)
+        }
+      }
+      rawData.position(0)
+      var _d: Array[Byte] = new Array(capacity)
+      rawData.get(_d)
+      println(s"Row #$pos saved: " + _d.toString)
+    }
+    override def toString: String = data.mkString(", ")
+  }
+
+  case class SelectSeq(val prev: List[SelectSeq] = Nil, val mod: Row => Boolean = (_) => true) {
     def all: SelectSeq =
       SelectSeq(this :: prev, (_) => true)
-    def equal(fName: String, m: Any): SelectSeq = {
-      val mtch = m match {
-        case x: Int => CellInt(x.toInt)
-        case x: Double => CellDouble(x.toDouble)
-        case x => CellText(x.toString)
+    def equal(fName: String, x: Any): SelectSeq = {
+      val cell = x match {
+        case y: Int => CellInt(y)
+        case y: Double => CellDouble(y)
+        case y: String => CellText(y)
       }
-      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) == mtch)
+      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) == cell)
     }
-    def lessThan: Unit = ()
-    def greaterThan: Unit = ()
-    def lessEq: Unit = ()
-    def greaterEq: Unit = ()
+    def lessThan(fName: String, x: Any): SelectSeq = {
+      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) < x)
+    }
+    def greaterThan(fName: String, x: Any): SelectSeq = {
+      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) > x)
+    }
+    def lessEq(fName: String, x: Any): SelectSeq = {
+      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) <= x)
+    }
+    def greaterEq(fName: String, x: Any): SelectSeq = {
+      SelectSeq(this :: prev, (_: Row).data(fieldOrder.indexOf(fName)) >= x)
+    }
     def confirm: List[Row] = {
       var lst: List[Row => Boolean] = List(mod)
       var res: List[Row] = Nil
@@ -72,9 +122,7 @@ abstract class Table {
         case x: Double => CellDouble(x.toDouble) :: lst
         case x => CellText(x.toString) :: lst
       }
-    data = Row(lst.reverse) :: data
-    Row(lst.reverse).save
+    data = Row(lst.reverse.toArray) :: data
+    Row(lst.reverse.toArray).save
   }
-  def all: SelectSeq =
-    SelectSeq(Nil, (_) => true)
 }
